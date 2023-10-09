5899cc780d5968e10478fb0f5e93e65d30251105
youce23 <138407517+youce23@users.noreply.github.com>
2023-10-09 19:33:42 +0900

    exeの動作不良を修正

--- build_pyinstaller.bat
+++ build_pyinstaller.bat
@@ -6,3 +6,6 @@ pipenv run pyinstaller dobble_maker_gui.py ^
     --onefile ^
     --noconsole ^
     --name=dobble_maker_gui.exe ^
+    --add-data ".venv/Lib/site-packages/galois/_databases/conway_polys.db;./galois/_databases" ^
+    --add-data ".venv/Lib/site-packages/galois/_databases/irreducible_polys.db;./galois/_databases" ^
+    --add-data ".venv/Lib/site-packages/galois/_databases/prime_factors.db;./galois/_databases" ^
