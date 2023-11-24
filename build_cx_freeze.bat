b8352b74ed359785b9c1c219874fc20749109eea
youce23 <138407517+youce23@users.noreply.github.com>
2023-11-24 17:59:56 +0900

    samplesのゴミがreleaseに混じらないようにバッチを修正

--- build_cx_freeze.bat
+++ build_cx_freeze.bat
@@ -3,9 +3,9 @@ pipenv sync --dev
 
 pipenv run python .\setup_cx_freeze.py build
 
-rmdir /s /q release
+rmdir /s /q samples release
 mkdir release
-git checkout release
+git checkout samples
 
 xcopy /y /i LICENSE release
 xcopy /y /s /i .\build\exe.win-amd64-3.11\* release
