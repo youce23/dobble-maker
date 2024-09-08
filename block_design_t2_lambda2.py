025aace1ea6710ec2ccf2d5bfbbe1ff150909016
youce23 <138407517+youce23@users.noreply.github.com>
2024-09-08 16:50:24 +0900

    算出済みのパラメータを定数化

--- block_design_t2_lambda2.py
+++ block_design_t2_lambda2.py
@@ -1,9 +1,11 @@
 import math
 from itertools import combinations
-from typing import Final, Literal
+from typing import Final, Literal, Sequence
 
 import numpy as np
 from tqdm import tqdm
+
+from dobble_maker import make_dobble_deck
 
 # t = 2, λ = 2において、例えば k = 3の場合、b = v = 4, r = k = 3である。
 # これをデッキの表現として以下のようにする。
@@ -45,31 +47,160 @@ from tqdm import tqdm
 # ここではλ = 2について考えるため、(3)に代入して
 #   v = k * (k -1) / 2 + 1  (6)
 
-t: Final[int] = 2
-lambda_: Literal[1, 2] = 2
-k: int = 4
-v: int = k * (k - 1) // lambda_ + 1  # 必ず割り切れる
-r: Final = k
-b: Final = v
 
-card_patterns_n: int = math.comb(v, k)  # 全カードパターン数
-deck_patterns_n: int = math.comb(card_patterns_n, b)  # 全デッキパターン数
-card_patterns_yield = combinations(range(v), k)
-deck_patterns_yield = combinations(card_patterns_yield, b)
-result_deck = None
-for deck in tqdm(deck_patterns_yield, desc="デッキ構築", total=deck_patterns_n):
+def _check_symmetric_bibd(deck_m: np.ndarray, k: int, lambda_: Literal[1, 2]):
+    r = k
+
+    assert all(deck_m.sum(axis=0) == r)  # デッキ全体での各シンボルの出現回数がrであることを確認
+    assert all(deck_m.sum(axis=1) == k)  # 各カードが有するシンボル数は必ずkになる
+    # 対角がk, 非対角がλになっているか確認
+    product = deck_m.dot(deck_m.T)
+    assert np.all(np.diag(product) == k) and np.all(product[~np.eye(product.shape[0], dtype=bool)] == lambda_)
+
+
+def _deck_to_deck_matrix(deck: Sequence[Sequence[int]]) -> np.ndarray:
+    v = len(set([s for card in deck for s in card]))
+
     deck_m = np.zeros((v, v), dtype=int)
     for card_i, card in enumerate(deck):
         for symbol_i in card:
             deck_m[card_i, symbol_i] = 1
-    if not all(deck_m.sum(axis=0) == r):
-        # デッキ全体での各シンボルの出現回数がrであることを確認
-        continue
-    assert all(deck_m.sum(axis=1) == k)  # 各カードが有するシンボル数は必ずkになる
-    # 対角がk, 非対角がλになっているか確認
-    product = deck_m.dot(deck_m.T)
-    if np.all(np.diag(product) == k) and np.all(product[~np.eye(product.shape[0], dtype=bool)] == lambda_):
-        result_deck = deck_m
-        break
 
-print(result_deck)
+    return deck_m
+
+
+def _deck_matrix_to_deck(deck_m: np.ndarray) -> list[tuple[int, ...]]:
+    deck = []
+    for i in range(deck_m.shape[0]):
+        card = tuple(np.where(deck_m[i] == 1)[0])
+        deck.append(card)
+
+    return deck
+
+
+def SymmetricBIBD_L2_K3() -> tuple[np.ndarray, list[tuple[int, int, int]]]:
+    # 算出済みの行列を定義しておく
+    # https://math.stackexchange.com/questions/3990502/algorithm-to-create-spot-it-dobble-cards-but-with-two-common-images-from-an
+    lambda_ = 2
+    k = 3
+    # t = 2
+    # v = k * (k - 1) // lambda_ + 1
+    # b = v
+    # r = k
+
+    deck = [(0, 1, 2), (0, 1, 3), (0, 2, 3), (1, 2, 3)]
+    deck_m = _deck_to_deck_matrix(deck)
+
+    # 検算
+    _check_symmetric_bibd(deck_m, k, lambda_)
+
+    return deck_m, deck
+
+
+def SymmetricBIBD_L2_K4() -> tuple[np.ndarray, list[tuple[int, int, int, int]]]:
+    # 算出済みの行列を定義しておく
+    # https://math.stackexchange.com/questions/3990502/algorithm-to-create-spot-it-dobble-cards-but-with-two-common-images-from-an
+    lambda_ = 2
+    k = 4
+    # t = 2
+    # v = k * (k - 1) // lambda_ + 1
+    # b = v
+    # r = k
+
+    deck = [(0, 1, 2, 3), (0, 1, 4, 5), (0, 2, 4, 6), (0, 3, 5, 6), (1, 2, 5, 6), (1, 3, 4, 6), (2, 3, 4, 5)]
+    deck_m = _deck_to_deck_matrix(deck)
+
+    # 検算
+    _check_symmetric_bibd(deck_m, k, lambda_)
+
+    return deck_m, deck
+
+
+def SymmetricBIBD_L2_K5() -> tuple[np.ndarray, list[tuple[int, int, int, int, int]]]:
+    # 算出済みの行列を定義しておく
+    # https://math.stackexchange.com/questions/3990502/algorithm-to-create-spot-it-dobble-cards-but-with-two-common-images-from-an
+    lambda_ = 2
+    k = 5
+    # t = 2
+    # v = k * (k - 1) // lambda_ + 1
+    # b = v
+    # r = k
+
+    deck = [
+        (0, 1, 2, 4, 7),
+        (0, 1, 3, 6, 10),
+        (0, 2, 5, 9, 10),
+        (0, 3, 7, 8, 9),
+        (0, 4, 5, 6, 8),
+        (1, 2, 3, 5, 8),
+        (1, 4, 8, 9, 10),
+        (1, 5, 6, 7, 9),
+        (2, 3, 4, 6, 9),
+        (2, 6, 7, 8, 10),
+        (3, 4, 5, 7, 10),
+    ]
+    deck_m = _deck_to_deck_matrix(deck)
+
+    # 検算
+    _check_symmetric_bibd(deck_m, k, lambda_)
+
+    return deck_m, deck
+
+
+def calc_symmetric_bibd_brute_force(lambda_: Literal[1, 2], k: int) -> tuple[np.ndarray, list[tuple[int, ...]]] | None:
+    # t: Final[int] = 2
+    v: int = k * (k - 1) // lambda_ + 1  # 必ず割り切れる
+    r: Final = k
+    b: Final = v
+
+    card_patterns_n: int = math.comb(v, k)  # 全カードパターン数
+    deck_patterns_n: int = math.comb(card_patterns_n, b)  # 全デッキパターン数
+    card_patterns_yield = combinations(range(v), k)
+    deck_patterns_yield = combinations(card_patterns_yield, b)
+    result_deck_m = None
+    for deck in tqdm(deck_patterns_yield, desc="デッキ構築", total=deck_patterns_n):
+        deck_m = _deck_to_deck_matrix(list(deck))
+
+        if not all(deck_m.sum(axis=0) == r):
+            # デッキ全体での各シンボルの出現回数がrであることを確認
+            continue
+        assert all(deck_m.sum(axis=1) == k)  # 各カードが有するシンボル数は必ずkになる
+        # 対角がk, 非対角がλになっているか確認
+        product = deck_m.dot(deck_m.T)
+        if np.all(np.diag(product) == k) and np.all(product[~np.eye(product.shape[0], dtype=bool)] == lambda_):
+            result_deck_m = deck_m
+            break
+
+    if result_deck_m is None:
+        return None
+
+    result_deck = _deck_matrix_to_deck(result_deck_m)
+
+    return result_deck_m, result_deck
+
+
+def calc_symmetric_bibd(lambda_: Literal[1, 2], k: int) -> tuple[np.ndarray, Sequence[Sequence[int]]] | None:
+    if lambda_ == 1:
+        deck, _ = make_dobble_deck(k)
+        deck_m = _deck_to_deck_matrix(deck)
+        _check_symmetric_bibd(deck_m, k, lambda_)
+        return deck_m, deck
+    elif lambda_ == 2 and k == 3:
+        return SymmetricBIBD_L2_K3()
+    elif lambda_ == 2 and k == 4:
+        return SymmetricBIBD_L2_K4()
+    elif lambda_ == 2 and k == 5:
+        return SymmetricBIBD_L2_K5()
+    else:
+        return calc_symmetric_bibd_brute_force(2, k)
+
+
+if __name__ == "__main__":
+    lambda_: Literal[1, 2] = 2
+    k: int = 5
+
+    ret = calc_symmetric_bibd(lambda_, k)
+    if ret is not None:
+        deck_m, deck = ret
+        print(deck_m)
+        print(deck)
